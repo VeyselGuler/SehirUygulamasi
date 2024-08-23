28a66871cf863949e60c9c3e5c49a04130a13d4a
VeyselGuler <veyselmor11@hotmail.com>
2024-08-23 19:16:40 +0300

    commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JavaScriptOdev.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
